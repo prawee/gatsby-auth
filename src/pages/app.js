94b86c49771ee9de9caa076724e3f7360801f41f
Prawee Wongsa <prawee@hotmail.com>
2019-09-10 04:12:01 +0700

    create app component

---
+++ src/pages/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Router } from '@reach/router'
+import Layout from '../components/layout'
+import Profile from '../components/profile'
+import Login from '../components/login'
+
+const app = () => {
+  return (
+    <Layout>
+      <Router>
+        <Profile path="/app/profile" />
+        <Login path="/app/login" />
+      </Router>
+    </Layout>
+  )
+}
+
+export default app
