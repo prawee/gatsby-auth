1bba587f3a970304777b0f03f6885799e912a348
Prawee Wongsa <prawee@hotmail.com>
2019-09-10 04:11:51 +0700

    create profile component

---
+++ src/components/profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const profile = () => {
+  return (
+    <>
+      <h1>Your Profile</h1>
+      <ul>
+        <li>Name: Your name will appear here</li>
+        <li>E-mail: And here goes the mail</li>
+      </ul>
+    </>
+  )
+}
+
+export default profile
