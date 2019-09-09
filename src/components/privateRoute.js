222ffc0b6c7114d08ce6fcaba30e129d793ba2b9
Prawee Wongsa <prawee@hotmail.com>
2019-09-10 04:17:56 +0700

    create privateRoute component

---
+++ src/components/privateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { navigate } from 'gatsby'
+import { isLoggedIn } from '../services/auth'
+
+class PrivateRoute extends React.Component {
+  componentDidMount() {
+    const { location } = this.props
+    let noOnLoginPage = location.pathname !== `/app/login`
+    if (!isLoggedIn() && noOnLoginPage) {
+      navigate("/app/login")
+      return null
+    }
+  }
+  render() {
+    const { component: Component, ...rest} = this.props
+    return <Component {...rest} />
+  }
+}
+
+export default PrivateRoute
