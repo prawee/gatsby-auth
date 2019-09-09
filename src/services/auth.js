15a58822d7884e36f57803fcc8b8276a3a247ea9
Prawee Wongsa <prawee@hotmail.com>
2019-09-10 03:56:35 +0700

    create auth services

---
+++ src/services/auth.js
@@ -0,0 +1,29 @@
+export const isBrowser = () => typeof window !== 'undefined'
+
+export const getUser = () =>
+  isBrowser() && window.localStorage.getItem("getsbyUser")
+  ? JSON.parse(window.localStorage.getItem("getsbyUser")) : {}
+
+const setUser = user =>
+  window.localStorage.setItem("gatsbyUser", JSON.stringify(user))
+
+export const handleLogin = ({ username, password }) => {
+  if (username === `pod` && password === 'test') {
+    return setUser({
+      username: `pod`,
+      name: `Prawee Wongsa`,
+      email: `prawee@hotmail.com`
+    })
+  }
+  return false
+}
+
+export const isLoggedIn = () => {
+  const user = getUser()
+  return !! user.username
+}
+
+export const logout = callback => {
+  setUser({})
+  callback()
+}
