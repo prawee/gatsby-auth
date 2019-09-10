ea185ca70f50a2179d0282a71f5e0092ad1bafe5
Prawee Wongsa <prawee@hotmail.com>
2019-09-10 12:53:43 +0700

    fixed name of gatsbyUser

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,8 @@
 export const isBrowser = () => typeof window !== 'undefined'
 
 export const getUser = () =>
-  isBrowser() && window.localStorage.getItem("getsbyUser")
-  ? JSON.parse(window.localStorage.getItem("getsbyUser")) : {}
+  isBrowser() && window.localStorage.getItem("gatsbyUser")
+  ? JSON.parse(window.localStorage.getItem("gatsbyUser")) : {}
 
 const setUser = user =>
   window.localStorage.setItem("gatsbyUser", JSON.stringify(user))
